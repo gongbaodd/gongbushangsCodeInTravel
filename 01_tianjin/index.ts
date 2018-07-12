fca461a9942110be07d7b39ac95c59a5b8e4ae9d
jian.gong <jian.gong@qunar.com>
2018-07-13 07:17:48 +0800

    addParticle

--- 01_tianjin/index.ts
+++ 01_tianjin/index.ts
@@ -1,4 +1,5 @@
 import * as Pixi from 'pixi.js';
+import { Particle } from './particle';
 
 const {
     innerHeight,
@@ -15,9 +16,18 @@ const renderer = Pixi.autoDetectRenderer(
 document.body.appendChild(renderer.view);
 const stage = new Pixi.Container();
 
+let particle: Particle;
+const addParticle = () => {
+    particle = new Particle();
+    particle.setPos(innerWidth/2, innerHeight/2);
+    particle.setV(0.4, -1.6);
+    stage.addChild(particle.sprite);
+}
 const loop = () => {
     requestAnimationFrame(loop);
     renderer.render(stage);
+    particle && particle.update();
 }
 
 loop();
+addParticle();
