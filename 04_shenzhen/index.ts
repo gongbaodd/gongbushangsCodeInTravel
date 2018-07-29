7120fd809129138ee8b9377d23b7ed7f29f77858
jian.gong <jian.gong@qunar.com>
2018-07-30 06:45:22 +0800

    compare

--- 04_shenzhen/index.ts
+++ 04_shenzhen/index.ts
@@ -1,77 +1,19 @@
-import * as THREE from 'three';
-import OrbitControls from 'three-orbitcontrols';
+import Controls from 'three-orbitcontrols';
+import { WebGLRenderer, PerspectiveCamera, Scene } from 'three';
 
 const {
     innerHeight: height,
     innerWidth: width,
 } = window;
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, width/ height, 1, 10000);
+const scene = new Scene();
+const camera = new PerspectiveCamera(70, width/height, 1, 10000);
 camera.position.z = 50;
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize(width, height);
+const controls = new Controls(camera, renderer.domElement);
 document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls(camera, renderer.domElement);
 
-let x = 0.1;
-let y = 0.1;
-let z = 0.1;
-let dt = 0.001;
-let max = 100;
+// dx = dt * a * (y - x)
+// dy = dt * x * (b - z) - y
+// dz = dt * (x * y - c * z)
 
-class LorenzGeo {
-    geo: THREE.Geometry;
-    mat = new THREE.PointsMaterial({
-        size: 1,
-        vertexColors: THREE.VertexColors,
-        depthTest: false,
-        opacity: 0.3,
-        sizeAttenuation:  false,
-        transparent: true,
-    });
-    mesh: THREE.Points;
-    len = 1000000;
-    makeMesh(scene: THREE.Scene) {
-        this.addVertices();
-        scene.remove(this.mesh);
-        this.mesh = new THREE.Points(this.geo, this.mat);
-        scene.add(this.mesh);
-    }
-    addVertices() {
-        this.geo = new THREE.Geometry();
-        
-        let a = 10;
-        let b = 28;
-        let c = 8/3;
-        let _r = 0;
-        let _g = 0;
-        let _b = 0;
-
-        for (let i = 0; i < max; i++) {
-            let color = new THREE.Color(`rgb(${(_r + i) % 255},${(_g + i) %255},${(_b + i) % 255})`);
-            let vertice = new THREE.Vector3(x, y, z);
-            this.geo.vertices.push(vertice);
-            this.geo.colors.push(color);
-            let dx = dt * a * (y - x);
-            let dy = dt * (x*(b - z) - y);
-            let dz = dt * (x * y - c * z);
-            x += dx;
-            y += dy;
-            z += dz;
-        }
-
-        max+=max;
-    }
-}
-
-const geo = new LorenzGeo();
-
-function draw() {
-    if (max < 1000000) {
-        geo.makeMesh(scene);
-    }
-    camera.lookAt(scene.position);
-    renderer.render(scene, camera);
-    requestAnimationFrame(draw);
-}
-draw();
